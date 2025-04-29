f53d3253620ad043ffd783abdad88e357d37b074
Juan Francisco Antonaya Lopez <juanfran.antonaya@escuelaestech.es>
2025-04-29 19:53:13 +0200

    Problemas solucionados?

--- setup_env.bat
+++ setup_env.bat
@@ -4,5 +4,6 @@ python -m venv venv
 call venv\Scripts\activate
 pip install --upgrade pip
 pip install -r requirements.txt
+call venv\Scripts\activate
 python prepare_models.py
 @echo Entorno virtual configurado, dependencias instaladas y modelos preparados.
